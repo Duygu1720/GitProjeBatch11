4a03ee4a0668497262ffb25069303c8d634b2ccc
Duygu <dygctnky918@gmail.com>
2020-11-13 15:02:24 +0300

    ikinci commit

--- src/BirinciGün.java
+++ src/BirinciGün.java
@@ -2,7 +2,10 @@
 public class BirinciGün {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("11:57 - Birinci Gün");
+		System.out.println("Ýkinci commit");
+		
 
 	}
 
