d6f60ee2a3f7e08fe996cc82120c9e7444619fdf
Duygu1720 <74558273+Duygu1720@users.noreply.github.com>
2020-11-19 09:29:13 +0300

    Update BirinciGün.java

--- src/BirinciGün.java
+++ src/BirinciGün.java
@@ -1,10 +1,11 @@
 
-public class BirinciGün {
+public class BirinciGÃ¼n {
 
 	public static void main(String[] args) {
 		
-		System.out.println("11:57 - Birinci Gün");
-		System.out.println("Ýkinci commit");
+		System.out.println("11:57 - Birinci GÃ¼n");
+		System.out.println("Ä°kinci commit");
+		System.out.println("10.11 - Ä°kinci GÃ¼n");
 		
 
 	}
